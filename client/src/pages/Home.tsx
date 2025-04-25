09fd856d3a1330e954b8c68d4ead25d62c904923
sprandel <pedrosprandel27@gmail.com>
2025-04-25 09:05:59 -0300

    feat: use books hook

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
-import { Book } from "../types/Book";
-import axiosInstance from "../axiosInstance";
 import BookCard from "../components/BookCard";
+import { useBooks } from "../hooks/useBooks";
 
 const Home: React.FC = () => {
-  const [books, setBooks] = useState<Book[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const response = await axiosInstance.get("http://localhost:3000/api/books");
-        setBooks(response.data);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError("An unexpected error occurred");
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchBooks();
-  }, []);
+  const { books, loading, error } = useBooks();
 
   if (loading) {
     return <div className="flex justify-center items-center h-screen">Loading...</div>;
