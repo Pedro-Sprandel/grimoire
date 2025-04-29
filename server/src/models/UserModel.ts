4df5669e503f7460478c0a74e582501693289913
sprandel <pedrosprandel27@gmail.com>
2025-04-29 02:30:49 -0300

    feat: register route

--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -21,31 +21,7 @@ const userSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  },
-  books: [
-    {
-      bookId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Book",
-        required: true,
-        unique: true
-      },
-      title: {
-        type: String,
-        trim: true
-      },
-      comment: {
-        type: String,
-        trim: true
-      },
-      rating: {
-        type: Number,
-        min: 0.5,
-        max: 5,
-        required: true
-      }
-    }
-  ]
+  }
 });
 
 const User = mongoose.model("User", userSchema);
